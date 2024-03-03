4f15ecf05390f64eeff02266b11a69a2eda36d68
REFH4CK <andres29ehr@gmail.com>
2024-03-03 03:00:49 -0400

    Fix all variables, define how const and introducing the land search by id

--- src/tracker/script.js
+++ src/tracker/script.js
@@ -1,13 +1,13 @@
-let baseUrl = "https://play.pixels.xyz/pixels/share/"; // Base URL to share view
-let minRange = 1; // Minimun lands number
-let maxRange = 5000; // Maximun lands number
+const baseUrl = "https://play.pixels.xyz/pixels/share/"; // Base URL to share view
+const minRange = 1; // Minimun lands number
+const maxRange = 5000; // Maximun lands number
 
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let leftFrame = document.getElementById("left_frame");
-let rightFrame = document.getElementById("right_frame");
+const leftFrame = document.getElementById("left_frame");
+const rightFrame = document.getElementById("right_frame");
 
 function randomsLand() {
     leftFrame.setAttribute('src',`${baseUrl}${randomNumber(minRange, maxRange)}`);
@@ -15,4 +15,17 @@ function randomsLand() {
 }
 
 window.addEventListener('load', randomsLand);
+
+const inputCodeLeft = document.getElementById('code-1');
+const btnLeft = document.getElementById('search_btn-left');
+const inputCodeRight = document.getElementById('code-2');
+const btnRight = document.getElementById('search_btn-right');
+
+btnLeft.addEventListener('click', () => {
+    leftFrame.setAttribute('src',`${baseUrl}${inputCodeLeft.value}`);
+});
+
+btnRight.addEventListener('click', () => {
+    rightFrame.setAttribute('src',`${baseUrl}${inputCodeRight.value}`);
+});
 
