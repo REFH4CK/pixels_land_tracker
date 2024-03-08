ba7dd09da9e4f5c5ad4643d62c3d56cd0105adc3
noveira <amylopez303@gmail.com>
2024-03-08 12:15:04 -0400

    fix the code indent

--- app.js
+++ app.js
@@ -7,19 +7,18 @@ function hideAd() {
 }
 
 function copyAddress() {
-    navigator.clipboard.writeText('0xd70e7713a2b21fbb525e2674aebfa7087a5d2d50')
-    .then(() => {
-      console.log('Texto copiado al portapapeles')
-      ad.classList.add('show_ad');
-      setTimeout(hideAd, 2000);
-    })
-    .catch(err => {
-      console.error('Error al copiar al portapapeles:', err)
-    })  
+  navigator.clipboard.writeText('0xd70e7713a2b21fbb525e2674aebfa7087a5d2d50')
+  .then(() => {
+    console.log('Texto copiado al portapapeles')
+    ad.classList.add('show_ad');
+    setTimeout(hideAd, 2000);
+  })
+  .catch(err => {
+    console.error('Error al copiar al portapapeles:', err)
+  })  
 }
 
 roninIcon.addEventListener('click', copyAddress);
-
 btnStarted.addEventListener('click', ()=>{
-   window.location.href = "https://refh4ck.github.io/pixels_land_tracker/src/tracker/tracker.html";
+  window.location.href = "https://refh4ck.github.io/pixels_land_tracker/src/tracker/tracker.html";
 });
